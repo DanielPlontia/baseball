43ff2503ae9ab846ed3e6493f037cde50463a03d
jongan.park <jongan.park@samsung.com>
2024-05-30 14:18:14 +0900

    First Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#Baseball
+---
+Baseball KATA
+
+C++ exception
---
+++ Sample-Test1/baseball_test.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../Project7/baseball.cpp"
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
